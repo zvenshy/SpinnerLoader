207e3610b68a57efb77b6f5f9a56ce4b8b42118e
zvenshy <zvenshy@gmail.com>
2015-08-09 01:59:23 +0800

    初步效果达成！

--- spinnerLoader.js
+++ spinnerLoader.js
@@ -10,11 +10,12 @@
 	var canvas = document.getElementById('loader'),
 		context = canvas.getContext('2d'),
 		count = 0,
+		isPushArray = 0,
 		
 		box = {
 			x: 300,
 			y: 300,
-			radius: 100
+			radius: 120
 		},
 		fixedCircle = {
 			x: [],
@@ -30,84 +31,119 @@
 			radius: 12
 		}
 	initFixed(box, fixedCircle.radius);
-	console.log(requestAnimationFrame)
-	requestAnimationFrame(drawMoveCircle);
+	//requestAnimationFrame(drawMoveCircle);
+	drawMoveCircle()
 
 	function initFixed(box, r, number) {
 		var n = number || 8;
 		var x = 0, y = 0;
 		var angle = Math.PI * 2 / n;
 		var i = 0;
+
 		context.beginPath();
 		context.arc(box.x, box.y, box.radius, 0, 2*Math.PI, true);
 		context.closePath();
+		
 		for (; i < n; i++) {
-			x = box.x + (box.radius - r) * Math.sin(angle * i),
-			y = box.y + (box.radius - r) * ( 1 - Math.cos(angle * i) );
-			fixedCircle.x.push(x);
-			fixedCircle.y.push(y);
+			x = box.x + (box.radius - r) * Math.sin(angle * i);
+			y = box.y - (box.radius - r) * Math.cos(angle * i);
+			if (!isPushArray) {
+				fixedCircle.x.push(x);
+				fixedCircle.y.push(y);
+				if(i === n - 1) {
+					isPushArray = 1;
+				}
+			}
+
 			context.save();
-			context.beginPath();
-			context.fillStyle = '#46B9FF';
-			context.arc(x, y, r, 0, 2*Math.PI, true);
-			context.closePath();
-			
-			context.fill();
+			drawCircle(x, y, r, '#46B9FF');
 			context.restore();
-		}		
+		}
+	}
+
+	function drawCircle(x, y, r, fillcolor) {
+		context.beginPath();
+		context.fillStyle = fillcolor;
+		context.arc(x, y, r, 0, 2*Math.PI, true);
+		context.closePath();
+		context.fill();
 	}
 
 	function drawMoveCircle() {
-		var moveAngle = 2 * Math.PI / 180,
+		var moveAngle = 2 * Math.PI / 180 ,
 			angle = ++ count * moveAngle,
 			r = moveCircle.radius,
 			R = fixedCircle.radius,
-			distance = r + R,
 			realRaduis = r,
-			num = isIntersect(),
-			x0, y0, sumSquaresSqrt,
+			
+			x0, y0, sumSquaresSqrt, distance,
 			p1 = {}, p2 = {}, p3 = {}, p4 = {}, p5 = {},
-			alpha, beta, gama,
-			x = box.x + (box.radius - fixedCircle.radius) * Math.sin(angle),
-			y = box.y + (box.radius - fixedCircle.radius) * ( 1 - Math.cos(angle) );
+			alpha, beta, gama, cta,
+			x = moveCircle.x = box.x + (box.radius - fixedCircle.radius) * Math.sin(angle),
+			y = moveCircle.y = box.y - (box.radius - fixedCircle.radius) * Math.cos(angle);
+			num = isIntersect();
+			//console.log(num)
 
-		context.clearRect(moveCircle.beforeX - 2 * r, moveCircle.beforeY - 2 * r, r * 4, r * 4);
+		;context.clearRect(moveCircle.beforeX - 2 * r, moveCircle.beforeY - 2 * r, r * 4, r * 4);
 		initFixed(box, fixedCircle.radius);
-		if ( num ) {
-			realRaduis = R + (1 - distance / (R + r));
+		if ( num !== false ) {
+			
 			x0 = fixedCircle.x[num];
 			y0 = fixedCircle.y[num];
-			sumSquaresSqrt = (Math.sqrt( (x0 - x) ^ 2 + (y0 -y) ^ 2 ));
-			p5.x = (x0 + x)/2;
-			p5.y = (y0 + y)/2;
-			alpha = Math.atan( (x0 - x) / (y0 - y) );
-			beta = Math.acos( (R - r) / sumSquaresSqrt ) - alpha;
+			distance = Math.sqrt( (moveCircle.x - fixedCircle.x[num]) * (moveCircle.x - fixedCircle.x[num]) +
+				(moveCircle.y - fixedCircle.y[num]) * (moveCircle.y - fixedCircle.y[num]) )
+			r = r + (1 - distance / (R + r)) * r;
+			sumSquaresSqrt = Math.sqrt( (x0 - x) * (x0 - x) + (y0 - y) * (y0 - y) );
+
+			alpha = Math.atan( (y0 -y) / (x0 -x) );
 			gama = Math.asin( (R - r) / sumSquaresSqrt );
+			beta = Math.PI / 2 - gama - alpha;
+			cta = alpha + gama;
 			p1 = {
-				x: x - r * Math.sin(beta),
-				y: y + r * Math.cos(beta)
-			};
-			p2 = {
-				x: x0 - R * Math.cos(beta),
-				y: y0 + R * Math.sin(beta)
-			};
-			p3 = {
-				x: x + r * Math.sin(gama - alpha),
-				y: y + r * Math.cos(gama - alpha)
-			};
-			p4 = {
-				x: x0 + R * Math.sin(alpha - gama),
-				y: y0 + R * Math.cos(alpha - gama)
+				x: x - r * Math.cos(beta),
+				y: y + r * Math.sin(beta)
 			};
 
-		} else {
-			context.beginPath();
+			p2 = {
+				x: x0 - R * Math.sin(cta),
+				y: y0 + R * Math.cos(cta)
+			};
+			p3 = {
+				x: x + r * Math.sin(beta),
+				y: y - r * Math.cos(beta)
+			};
+			p4 = {
+				x: x0 + R * Math.cos(cta),
+				y: y0 - R * Math.sin(cta)
+			};
+			p5 = {
+				x: (x0 + x) / 2,
+				y: (y0 + y) / 2
+			};
+
+			context.save();
+			context.moveTo(p1.x, p1.y);
+			context.quadraticCurveTo(p5.x, p5.y, p2.x, p2.y);
+			context.lineTo(p4.x, p4.y);
+			context.quadraticCurveTo(p5.x, p5.y, p3.x, p3.y);
+			context.lineTo(p1.x, p1.y);
 			context.fillStyle = '#46B9FF';
-			context.arc(x, y, moveCircle.radius, 0, 2 * Math.PI, true);
-			context.closePath();
 			context.fill();
+			context.restore();
+			
 		}
-		moveCircle.beforeX = x;
+
+		drawCircle(x, y, r, '#46B9FF');
+/*		drawCircle(moveCircle.x, moveCircle.y, 1, 'yellow');
+		drawCircle(fixedCircle.x[0], fixedCircle.y[0], 1, 'red');
+		console.log( moveCircle.x - fixedCircle.x[0] , Math.pow(moveCircle.y - fixedCircle.y[0]) )*/
+
+/*			drawCircle(p1.x, p1.y, 2, "yellow")
+			drawCircle(p2.x, p2.y, 3, "gray")
+			drawCircle(p3.x, p3.y, 2, "black")
+			drawCircle(p4.x, p4.y, 2, "blue")
+			drawCircle(p5.x, p5.y, 2, "yellow")
+*/		moveCircle.beforeX = x;
 		moveCircle.beforeY = y;
 		requestAnimationFrame(drawMoveCircle);
 	}
@@ -115,8 +151,8 @@
 	function isIntersect() {
 		var i = 0, len = fixedCircle.x.length;
 		for (; i < len; i ++) {
-			if (Math.sqrt( (moveCircle.x - fixedCircle.x[i]) ^ 2 
-				+ (moveCircle.y - fixedCircle.y[i]) ^ 2) < 
+			if (Math.sqrt( (moveCircle.x - fixedCircle.x[i]) * (moveCircle.x - fixedCircle.x[i]) +
+				(moveCircle.y - fixedCircle.y[i]) * (moveCircle.y - fixedCircle.y[i]) ) < 
 				(moveCircle.radius + fixedCircle.radius) ) {
 				break;
 			}
